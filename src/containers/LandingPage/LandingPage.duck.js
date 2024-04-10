8f60055eac3ee9dd2c08afc4f5831eccebfee9e7
Sari Sharetribe <sari@sharetribe.com>
2024-04-10 16:19:35 +0300

    Update LandingPage.duck.js to fetch recommended listings

--- src/containers/LandingPage/LandingPage.duck.js
+++ src/containers/LandingPage/LandingPage.duck.js
@@ -1,7 +1,60 @@
 import { fetchPageAssets } from '../../ducks/hostedAssets.duck';
+import { searchListings } from '../SearchPage/SearchPage.duck';
+import { createImageVariantConfig } from '../../util/sdkLoader';
 export const ASSET_NAME = 'landing-page';
+export const recommendedSectionId = 'recommended-listings';
 
-export const loadData = (params, search) => dispatch => {
+const getListingParams = (config, listingIds) => {
+  const {
+    aspectWidth = 1,
+    aspectHeight = 1,
+    variantPrefix = 'listing-card',
+  } = config.layout.listingImage;
+
+  const aspectRatio = aspectHeight / aspectWidth;
+
+  return {
+    ids: listingIds,
+    include: ['author', 'images'],
+    'fields.listing': [
+      'title',
+      'geolocation',
+      'price',
+      'publicData.listingType',
+      'publicData.transactionProcessAlias',
+      'publicData.unitType',
+      // These help rendering of 'purchase' listings,
+      // when transitioning from search page to listing page
+      'publicData.pickupEnabled',
+      'publicData.shippingEnabled',
+    ],
+    'fields.user': ['profile.displayName', 'profile.abbreviatedName'],
+    'fields.image': [
+      'variants.scaled-small',
+      'variants.scaled-medium',
+      `variants.${variantPrefix}`,
+      `variants.${variantPrefix}-2x`,
+    ],
+    ...createImageVariantConfig(`${variantPrefix}`, 400, aspectRatio),
+    ...createImageVariantConfig(`${variantPrefix}-2x`, 800, aspectRatio),
+    'limit.images': 1,
+  };
+};
+
+export const loadData = (params, search, config) => dispatch => {
   const pageAsset = { landingPage: `content/pages/${ASSET_NAME}.json` };
-  return dispatch(fetchPageAssets(pageAsset, true));
+
+  return dispatch(fetchPageAssets(pageAsset, true)).then(assetResp => {
+    // Get listing ids from custom recommended listings section
+    const customSection = assetResp.landingPage?.data?.sections.find(
+      s => s.sectionId === recommendedSectionId
+    );
+
+    if (customSection) {
+      const recommendedListingIds = customSection?.blocks.map(b => b.blockName);
+      const listingParams = getListingParams(config, recommendedListingIds);
+      console.log('has custom section, dispatching now!')
+      dispatch(searchListings(listingParams, config));
+    }
+  });
 };
